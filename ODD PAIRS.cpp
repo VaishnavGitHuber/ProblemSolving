2664330eae3058375fec8abf6abb2a28d7f0283f
Vaishnav Krishna P <124416940+VaishnavGitHuber@users.noreply.github.com>
2024-02-02 05:58:28 +0530

    CODE CHEF 02/02/2024

---
+++ ODD PAIRS.cpp
@@ -0,0 +1,15 @@
+// Question: https://www.codechef.com/practice/course/1-star-difficulty-problems/DIFF1200/problems/ODDPAIRS?tab=statement
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    cout << n * n/2 << endl;
+	}
+}
+
+
